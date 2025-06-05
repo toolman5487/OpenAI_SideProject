b91fee7a3c1258beb1ea75e3e85c6ce9d932644b
ToolManHsu <willy548798@gmail.com>
2025-06-05 15:06:23 +0800

    Initial Commit

---
+++ OpenAI_SideProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenAI_SideProject
+//
+//  Created by Willy Hsu on 2025/6/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
